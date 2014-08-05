3ce69a285fd8263f28cafaa3d6fb1509c359f4f1
George Roberts <groberts@purityconsulting.com>
2014-08-05 14:56:14 -0500

    Convert to WP8 and WP8.1

---
+++ src/Purity.QR.WindowsPhone81/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿//
+// Copyright (c) 2010-2011 Jeff Wilcox
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+[assembly: AssemblyTitle("Purity QR Control")]
+[assembly: AssemblyDescription("A QRCodeScanner control for Windows Phone 8.1")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Purity QR Control")]
+[assembly: AssemblyCopyright("Copyright ©  2012 Jeff Wilcox; Portions Copyright ©  2014 Purity Consulting")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0f898eec-f105-466f-95bc-83e817084fed")]
+
+[assembly: AssemblyVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
+
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
